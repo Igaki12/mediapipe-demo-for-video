333c979a958ed140d87e445071dee8aa517a2e90
Igaki12 <igakin8.19971222@gmail.com>
2024-11-25 13:56:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ videoSelector.addEventListener("change", async (event) => {
             video.currentTime = i * video.duration / 10;
             video.addEventListener("seeked", async () => { 
                 console.log("video currentTime : ", video.currentTime);
-                const canvas = document.getElementById("canvas" + i);
+                const canvas = document.getElementById("videoFrameCanvas" + i);
                 canvas.width = video.videoWidth * 0.3;
                 canvas.height = video.videoHeight * 0.3;
                 const ctx = canvas.getContext("2d");
